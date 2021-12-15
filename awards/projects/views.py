263b0eeb6499aff2e031161ad718ac5b9ff99362
Nabuuso <wagabaliz@gmail.com>
2021-12-15 12:29:40 +0300

    Dashboard banner, login content, register page

--- awards/projects/views.py
+++ awards/projects/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render,redirect
 from django.views import View
+from django.http import JsonResponse
+from . models import *
+from django.core.mail import EmailMessage
+from django.contrib import auth
+from django.contrib import messages
 
 # Create your views here.
 class DashboardView(View):
     def get(self,request):
-        return render(request,'home/dashboard.html')
+        return render(request,'home/index.html')
