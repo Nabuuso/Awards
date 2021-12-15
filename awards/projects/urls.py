297cc6bbf3107e571335ab921511ec1b631c75d4
Nabuuso <wagabaliz@gmail.com>
2021-12-15 12:45:45 +0300

    Login and logout views

--- awards/projects/urls.py
+++ awards/projects/urls.py
@@ -3,5 +3,8 @@ from .views import *
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth.decorators import login_required
 urlpatterns = [
-    path('',DashboardView.as_view(),name='dashboard')
+    path('',DashboardView.as_view(),name='dashboard'),
+    path('register',csrf_exempt(RegisterView.as_view()),name="register"),
+    path('login',LoginView.as_view(),name="login"),
+    path('logout',LogoutView.as_view(),name='logout'),
 ]
--- awards/projects/views.py
+++ awards/projects/views.py
@@ -10,3 +10,51 @@ from django.contrib import messages
 class DashboardView(View):
     def get(self,request):
         return render(request,'home/index.html')
+class LoginView(View):
+    def get(self,request):
+        return render(request,'authentication/login.html')
+    def post(self,request):
+        email = request.POST['email']
+        password = request.POST['password']
+        if email and password:
+            user = auth.authenticate(email=email,password=password)
+            if user:
+                auth.login(request,user)
+                return redirect('/dashboard')
+            else:
+                messages.error(request,'Invalid login')
+                return render(request,'authentication/login.html')
+        messages.error(request,'Invalid login credentials')
+        return render(request,'authentication/login.html')
+class LogoutView(View):
+    def post(self,request):
+        auth.logout(request)
+        messages.success(request,"You have successfully logged out")
+        return redirect('/')
+####REGISTER VIEW
+class RegisterView(View):
+    def get(self,request):
+        return render(request,'authentication/register.html')
+    def post(self,request):
+        username = request.POST['username']
+        email = request.POST['email']
+        full_name = request.POST['full_name']
+        password = request.POST['password']
+        if not Profile.objects.filter(username=username).exists():
+            if not Profile.objects.filter(email=email).exists():
+                profile = Profile.objects.create_user(username=username,email=email,password=password,full_name=full_name)
+                print(profile)
+                # profile.set_password(password)
+                profile.save()
+                email = EmailMessage(
+                    'InstaBlog Account',
+                    'InstaBlog account created successfully',
+                    'liznabuuso@gmail.com',
+                    [email]
+                )
+                email.send(fail_silently=True)
+                return JsonResponse({"message":"Account created successfully","status":201},status=201)
+            else:
+                return JsonResponse({"error":"Username already taken","status":400},status=400)
+        else:
+            return JsonResponse({"error":"Email already taken","status":400},status=400)
