28840d3f814b7aaab77fc51ecf77239621e15380
Nabuuso <wagabaliz@gmail.com>
2021-12-15 08:40:46 +0300

    Dashboad navigation configuration, urls & static files

---
+++ awards/projects/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from .views import *
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.decorators import login_required
+urlpatterns = [
+]
