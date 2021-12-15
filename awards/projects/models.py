53c1fed588dda8c9edf770f987e51135ad2068bc
Nabuuso <wagabaliz@gmail.com>
2021-12-15 16:15:12 +0300

    Projects & Rating models

--- awards/projects/models.py
+++ awards/projects/models.py
@@ -35,3 +35,38 @@ class Profile(AbstractBaseUser,PermissionsMixin):
         return self.full_name
     class Meta:
         ordering = ['-created_date']
+class Project(models.Model):
+    image = models.ImageField(upload_to="images/project",null=True)
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    profile = models.ForeignKey(Profile,on_delete=models.CASCADE,related_name="profile_projects")
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now_add=True)
+    design_rating = models.IntegerField(default=0)
+    usability_rating = models.IntegerField(default=0)
+    content_rating = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.image_name
+    def save_image(self,*args,**kwargs):
+        super().save(*args,**kwargs)
+    def delete_image(self,pk):
+        img = Project.objects.get(pk=pk)
+        if len(img.image) > 0:
+            os.remove(img.image.path)
+        img.delete()
+    def update_caption(self,*args,**kwargs):
+        super().update(*args,**kwargs)
+    
+    class Meta:
+        ordering = ['-created_date']
+class Rating(models.Model):
+    design_rating = models.IntegerField(default=0)
+    usability_rating = models.IntegerField(default=0)
+    content_rating = models.IntegerField(default=0)
+    project = models.ForeignKey(Project,on_delete=models.DO_NOTHING)
+    profile = models.ForeignKey(Profile,on_delete=models.DO_NOTHING)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now_add=True)
+    class Meta:
+        ordering = ['-created_date']
