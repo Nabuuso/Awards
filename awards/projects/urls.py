4fee36b11653e3b898707fa0c7e681d83539bafb
Nabuuso <wagabaliz@gmail.com>
2021-12-15 09:10:04 +0300

    projects urls and dashboard view

--- awards/projects/urls.py
+++ awards/projects/urls.py
@@ -3,4 +3,5 @@ from .views import *
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth.decorators import login_required
 urlpatterns = [
+    path('',DashboardView.as_view(),name='dashboard')
 ]
